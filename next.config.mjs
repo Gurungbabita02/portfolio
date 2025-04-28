255524c278cc9f0bfe9156fd463f248ada7a34bd
gurungbabita02 <babita.grg121@gmail.com>
2025-04-28 10:40:12 +0530

    new changes-- 28 april

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects(){
+        return[
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'www.almuqeet.net',
+              },
+            ],
+            destination: 'https://almuqeet.net/:path*',
+            permanent: true,
+          },
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'http://almuqeet.net',
+              },
+            ],
+            destination: 'https://almuqeet.net/:path*',
+            permanent: true,
+          },
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value:'almuqeet.net',
+              },
+            ],
+            destination: 'https://almuqeet.net/:path*',
+            permanent: true,
+          },
+        ]
+      }
+};
 
 export default nextConfig;
